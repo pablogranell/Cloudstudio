902b23270e39dea833b844224bddd8a978db603b
Pablo Granell <pablogranell2002@gmail.com>
2024-06-11 19:32:04 +0200

    traer e imprimir diccionario

--- Share/camaraLogger.py
+++ Share/camaraLogger.py
@@ -3,8 +3,10 @@ import time
 from nerfstudio.viewer.viewer_elements import ViewerControl
 import nerfstudio.viewer.viewer as viewer
 
-mainViewer = viewer.Viewer()
-viewer_server = viewer.ViewerServer(mainViewer)
+viewer_server = viewer.ViewerServer()
+clients = viewer_server.get_clients()
+for client_id, client_handle in clients.items():
+    print(f"Client {client_id} is connected with handle {client_handle}")
 viewer_control = ViewerControl(viewer_server)
 while True :
     time.sleep(0.01)
