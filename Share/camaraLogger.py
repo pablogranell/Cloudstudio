79732ad2fbf3dfeff5503b63483d27ee290b38cf
Pablo Granell <pablogranell2002@gmail.com>
2024-06-11 18:21:48 +0200

    testing import

--- Share/camaraLogger.py
+++ Share/camaraLogger.py
@@ -1,4 +1,7 @@
 from os import wait
+from Cloudstudio import nerfstudio
+from nerfstudio import viewer
+from nerfstudio.viewer import viewer_elements
 from Cloudstudio.nerfstudio.viewer.viewer_elements import ViewerControl
 
 
