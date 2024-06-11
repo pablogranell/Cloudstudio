d5136420723a51e8b0a6df0d40330f606fdca175
Pablo Granell <pablogranell2002@gmail.com>
2024-06-11 18:33:36 +0200

    ajustado tiempo

--- Share/camaraLogger.py
+++ Share/camaraLogger.py
@@ -1,4 +1,5 @@
 from os import wait
+import time
 #from Cloudstudio import nerfstudio
 #from nerfstudio import viewer
 #from nerfstudio.viewer import viewer_elements
@@ -7,7 +8,7 @@ from nerfstudio.viewer.viewer_elements import ViewerControl
 
 viewer_control = ViewerControl()
 while True :
-    wait(1)
+    time.sleep(0.01)
     cameraPosition = viewer_control.get_camera(100, 100)
     if cameraPosition is not None:
         print(f"Camera Position: {cameraPosition}")
