27533ee7c7411552e30c396b67939dd23665b4cf
Pablo Granell <pablogranell2002@gmail.com>
2024-06-15 00:16:31 +0200

    python mistake

--- nerfstudio/viewer/viewer.py
+++ nerfstudio/viewer/viewer.py
@@ -304,7 +304,7 @@ class Viewer:
                 #CONSOLE.log(f"Cliente: {client.client_id}")
                 CONSOLE.log(f"ID del cliente: {client.client_id}")
                 #Fix client ID 0
-                if client.client_id == 0 & len(clients) > 1:
+                if client.client_id == 0 and len(clients) > 1:
                     CONSOLE.log("Cliente 0")
                     for id in clients:
                         if id != 0:
