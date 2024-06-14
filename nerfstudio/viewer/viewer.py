e4384807e540bbfd10ac7bc55432a68b4e1fa070
Pablo Granell <pablogranell2002@gmail.com>
2024-06-14 20:12:51 +0200

    bug fix

--- nerfstudio/viewer/viewer.py
+++ nerfstudio/viewer/viewer.py
@@ -304,14 +304,13 @@ class Viewer:
                 if len(clients) > 1:
                     for id in clients:
                         if id != 0:
-                            #CONSOLE.log(f"Cliente: {id}")
-                            #camera_state = self.get_camera_state(client)
-                            #self.render_statemachines[client.client_id].action(RenderAction("move", camera_state))
                             CONSOLE.print(clients[id])
+                            CONSOLE.print("Camera del cliente")
                             CONSOLE.print(clients[id].camera)
                             clients[id].camera.position = clients[0].camera.position
                             clients[id].camera.wxyz = clients[0].camera.wxyz
-                            #self.render_statemachines[client.client_id].action(RenderAction("rerender", camera_state))
+                            camera_state = self.get_camera_state(clients[0])
+                            self.render_statemachines[id].action(RenderAction("move", camera_state))
 
     def make_stats_markdown(self, step: Optional[int], res: Optional[str]) -> str:
         # if either are None, read it from the current stats_markdown content
