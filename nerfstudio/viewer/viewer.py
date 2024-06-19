46c754bd1584c8d954e7c086cca99a95ee9a86c5
Pablo Granell <pablogranell2002@gmail.com>
2024-06-19 16:07:43 +0200

    adjuster timer

--- nerfstudio/viewer/viewer.py
+++ nerfstudio/viewer/viewer.py
@@ -64,12 +64,12 @@ def toggle_sincronizacion():
 def reset_updating():
     global updating
     updating = False
-    CONSOLE.print(f"reset updating, now: {updating}")
+    #CONSOLE.print(f"reset updating, now: {updating}")
 
 def toggle_updating():
     global updating
     updating = True
-    CONSOLE.print(f"updating: {updating}")
+    #CONSOLE.print(f"updating: {updating}")
 
 @decorate_all([check_main_thread])
 class Viewer:
@@ -321,7 +321,7 @@ class Viewer:
                                 self.render_statemachines[id].action(RenderAction("move", camera_state))
                                 clients[id].camera.position = client.camera.position
                                 clients[id].camera.wxyz = client.camera.wxyz
-                                threading.Timer(0.1, reset_updating).start()
+                                threading.Timer(0.2, reset_updating).start()
 
     def make_stats_markdown(self, step: Optional[int], res: Optional[str]) -> str:
         # if either are None, read it from the current stats_markdown content
